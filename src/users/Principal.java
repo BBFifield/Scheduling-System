db1456a5ae268022e3ee8615f7b6b06660053d21
tsb050 <tsb0550@mun.ca>
2017-11-17 10:34:46 -0330

    implemented princpal and room classes

---
+++ src/users/Principal.java
@@ -0,0 +1,20 @@
+package users;
+
+import schedule.Room;
+
+public class Principal extends User {
+	
+	public Principal(String name, String username, String emailAddress, String permission, int requestCountWeek, System system){
+		super(name, username, emailAddress, permission, requestCountWeek, system);
+	}
+	
+	public void addUser(User user){
+		
+	}
+	public void addRoom (Room room){
+		
+	}
+	public Booking Approvebooking(){
+		
+	} 
+}
