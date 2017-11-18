11e704311cb8e60b977176c05c732e73c9ac6798
Brandon Fifield <bbf522@mun.ca>
2017-11-18 13:13:57 -0330

    Added bin files to be uploaded

--- src/graphicsComponents/BookingsFrame.java
+++ src/graphicsComponents/BookingsFrame.java
@@ -3,19 +3,25 @@ package graphicsComponents;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.GridLayout;
+import javax.swing.JCheckBox;
 
 public class BookingsFrame {
 	JFrame frame;
-	
+
 	public BookingsFrame() {
 		frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(500, 500));
+		frame.add(panel);
+		frame.setAlwaysOnTop(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(400,400));
-		
-		JButton btnNewButton = new JButton("New button");
-		frame.getContentPane().add(btnNewButton, BorderLayout.NORTH);
+		frame.setPreferredSize(new Dimension(400, 400));
 		frame.setVisible(true);
+		frame.dispose();
 	}
 }
